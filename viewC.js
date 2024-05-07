cac904b372d1d2123707ce9ef0be9fbb90ae6261
Michael Hart <hartmt@appstate.edu>
2024-05-07 14:58:32 -0400

    Can now select options get the value

--- viewC.js
+++ viewC.js
@@ -6,15 +6,36 @@ class ViewC {
         .style('width','50%')
         .style('height','50%');
 
-		const select = div.append('select')
-		.style('width', '100px')
-		.style('height', '50px')
-		.style('position', 'absolute')
-		.style('left', '49.5%')
-		.style('top', '56%')
-	
-        const svg = div.append('svg');
-		
+		d3.csv("Sub_Genre_df.csv").then(function(data) {
+
+			const select = div.append('select')
+				.style('width', '230px')
+				.style('height', '35px')
+				.style('position', 'relative')
+				.style('left', '2%')
+				.style('top', '2%')
+				.attr('id', 'selectButton');
+
+			d3.select('#selectButton').selectAll('myOptions')
+				.data(data)
+				.join('option')
+				.text(function (d) { return d['Title']; }) // text showed in the menu
+				.attr("value", function (d) {return d['Title'];})
+			
+			d3.select("#selectButton").on("change", function(d) {
+				// recover the option that has been chosen
+				var selectedOption = d3.select(this).property("value")
+				// run the updateChart function with this selected option
+				update(selectedOption)
+			});
+
+			const svg = div.append('svg')
+				.style('position', 'relative')
+				.style('left', '0%')
+				.style('top', '4%')
+				.attr('width','100%')
+				.attr('height','89%');
+		});
 		
 
         d3.csv("Books_df.csv").then(function(data) {
@@ -23,5 +44,11 @@ class ViewC {
 
 			
 		});
+
+		function update(selectedGroup){
+
+			console.log(selectedGroup);
+
+		}
     }
 }
