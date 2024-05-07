7607d6405f3434120b906555b15ae7f469c005b4
Michael Hart <hartmt@appstate.edu>
2024-05-07 17:32:14 -0400

    Added base functionality to bubble plot

--- viewC.js
+++ viewC.js
@@ -6,7 +6,20 @@ class ViewC {
         .style('width','50%')
         .style('height','50%');
 
-		d3.csv("Sub_Genre_df.csv").then(function(data) {
+        d3.csv("Books_df.csv").then(function(data) {
+
+			let i = 0;
+			const subs = [];
+			const auths = [];
+
+			while(i < data.length)
+			{
+				subs[i] = data[i]['Sub Genre'];
+				i++;
+			}
+
+			const subgenres = removeExtras(subs);
+			subgenres.sort();
 
 			const select = div.append('select')
 				.style('width', '230px')
@@ -17,38 +30,81 @@ class ViewC {
 				.attr('id', 'selectButton');
 
 			d3.select('#selectButton').selectAll('myOptions')
-				.data(data)
+				.data(subgenres)
 				.join('option')
-				.text(function (d) { return d['Title']; }) // text showed in the menu
-				.attr("value", function (d) {return d['Title'];})
+				.text(function (d) { return d;}) // text showed in the menu
+				.attr("value", function (d) {return d;})
 			
-			d3.select("#selectButton").on("change", function(d) {
-				// recover the option that has been chosen
-				var selectedOption = d3.select(this).property("value")
-				// run the updateChart function with this selected option
-				update(selectedOption)
-			});
-
 			const svg = div.append('svg')
 				.style('position', 'relative')
 				.style('left', '0%')
 				.style('top', '4%')
 				.attr('width','100%')
 				.attr('height','89%');
-		});
-		
 
-        d3.csv("Books_df.csv").then(function(data) {
-
-
-
+			let current = 'Action & Adventure';
+			const curBooks = data.filter((book) => book['Sub Genre'] == current);
 			
+			i = 0;
+			while(i < curBooks.length)
+			{
+				auths[i] = curBooks[i]['Author'];
+				i++;
+			}
+			const authors = removeExtras(auths);
+			authors.sort;
+			console.log(authors);
+
+			const xScale = d3.scalePoint().domain(authors).range([100, 750])
+			const yScale = d3.scaleLinear().domain([0, 5]).range([250, 50])
+
+			const bubbles = svg.append('g').selectAll('circle')
+				.data(curBooks)
+				.join('circle')
+				.attr('cx', function(d){return xScale(d['Author'])})
+				.attr('cy', function(d){return yScale(d['Rating'])})
+				.attr('r', 5)
+				.attr('fill', 'none')
+				.attr('stroke', 'grey')
+				.attr('stroke-width', 3);
+
+			function update(selectedGroup){
+					console.log(selectedGroup);
+					//change bubbles here based on drop down selection
+					const curBooks = data.filter((book) => book['Sub Genre'] == selectedGroup);
+					const auths = [];
+					let i = 0;
+					while(i < curBooks.length)
+					{
+						auths[i] = curBooks[i]['Author'];
+						i++;
+					}
+					const authors = removeExtras(auths);
+					authors.sort;
+
+					const xScale = d3.scalePoint().domain(authors).range([100, 750])
+					const yScale = d3.scaleLinear().domain([0, 5]).range([250, 50])
+
+					bubbles
+						.data(curBooks)
+						.join('circle')
+						.attr('cx', function(d){return xScale(d['Author'])})
+						.attr('cy', function(d){return yScale(d['Rating'])})
+						.attr('r', 5)
+						.attr('fill', 'none')
+						.attr('stroke', 'grey')
+						.attr('stroke-width', 3);
+			}
+
+			d3.select("#selectButton").on("change", function() {	
+				var selectedOption = d3.select(this).property("value")
+				update(selectedOption)
+			});
 		});
 
-		function update(selectedGroup){
-
-			console.log(selectedGroup);
-
+		function removeExtras(data){
+			return data.filter((value, index) => data.indexOf(value) === index);
 		}
+
     }
 }
