9ff9c4d8129faa9127c4cb798c207fcf5fdd5d52
Michael Hart <hartmt@appstate.edu>
2024-05-08 20:29:03 -0400

    Finished bubble chart

--- viewC.js
+++ viewC.js
@@ -32,8 +32,8 @@ class ViewC {
 			d3.select('#selectButton').selectAll('myOptions')
 				.data(subgenres)
 				.join('option')
-				.text(function (d) { return d;}) // text showed in the menu
-				.attr("value", function (d) {return d;})
+				.text(function (d) { return d;})
+				.attr("value", function (d) {return d;});
 			
 			const svg = div.append('svg')
 				.style('position', 'relative')
@@ -53,47 +53,83 @@ class ViewC {
 			}
 			const authors = removeExtras(auths);
 			authors.sort;
-			console.log(authors);
+			
+			const bubbleList = makeBubbleList(authors, curBooks);
 
-			const xScale = d3.scalePoint().domain(authors).range([100, 750])
-			const yScale = d3.scaleLinear().domain([0, 5]).range([250, 50])
+			const xScale = d3.scalePoint().domain(authors).range([50, 700])
+			const yScale = d3.scaleLinear().domain([0, 5]).range([320, 50])
+			const colorScale = d3.scaleLinear().domain([1, 5]).range(["#4dc9e6", "#210cae"]);
+			const sizeScale = d3.scaleLinear().domain([Math.min(...(bubbleList.map(d => d.numRatings))), Math.max(...(bubbleList.map(d => d.numRatings)))]).range([5, 40])
 
-			const bubbles = svg.append('g').selectAll('circle')
-				.data(curBooks)
+			var bubbles = svg.append('g').attr('class','bubbles').selectAll('circle')
+				.data(bubbleList)
 				.join('circle')
-				.attr('cx', function(d){return xScale(d['Author'])})
-				.attr('cy', function(d){return yScale(d['Rating'])})
-				.attr('r', 5)
-				.attr('fill', 'none')
-				.attr('stroke', 'grey')
-				.attr('stroke-width', 3);
+				.attr('cx', function(d){return xScale(d['author'])})
+				.attr('cy', function(d){return yScale(d['avgRating'])})
+				.attr('r', function(d){return sizeScale(d['numRatings'])})
+				.attr('fill', function(d){return colorScale(d['booksWritten'])})
+				.attr('stroke', 'none')
+				.attr('stroke-width', 3)
+				.attr('opacity', 0.7)
+
+			var xaxis = svg.append('g')
+				.attr('transform', 'translate(0, 320)')
+				.call(d3.axisBottom(xScale))
+				.attr("class", "xaxis")
+				.selectAll("text")
+				.style("text-anchor", "end")
+				.attr("dx", "-.8em")
+				.attr("dy", ".15em")
+				.attr("transform", "rotate(-65)")
+				.text(function(d){return d.substring(0,13) + '.'});
+
+			var yaxis = svg.append('g')
+				.attr('transform', 'translate(50, 0)')
+				.call(d3.axisLeft(yScale));
 
 			function update(selectedGroup){
-					console.log(selectedGroup);
-					//change bubbles here based on drop down selection
-					const curBooks = data.filter((book) => book['Sub Genre'] == selectedGroup);
-					const auths = [];
-					let i = 0;
-					while(i < curBooks.length)
-					{
-						auths[i] = curBooks[i]['Author'];
-						i++;
-					}
-					const authors = removeExtras(auths);
-					authors.sort;
+				
+				const curBooks = data.filter((book) => book['Sub Genre'] == selectedGroup);
+			
+				i = 0;
+				while(i < curBooks.length)
+				{
+					auths[i] = curBooks[i]['Author'];
+					i++;
+				}
+				const authors = removeExtras(auths);
+				authors.sort;
+				
+				const bubbleList = makeBubbleList(authors, curBooks);
+				
+				const xScale = d3.scalePoint().domain(authors).range([50, 700])
+				const sizeScale = d3.scaleLinear().domain([Math.min(...(bubbleList.map(d => d.numRatings))), Math.max(...(bubbleList.map(d => d.numRatings)))]).range([5, 40])
+					
+				d3.selectAll('g.bubbles').remove();
+				d3.selectAll('circle').remove();
+				bubbles = svg.append('g').attr('class','bubbles').selectAll('circle')
+					.data(bubbleList)
+					.join('circle')
+					.attr('cx', function(d){return xScale(d['author'])})
+					.attr('cy', function(d){return yScale(d['avgRating'])})
+					.attr('r', function(d){return sizeScale(d['numRatings'])})
+					.attr('fill', function(d){return colorScale(d['booksWritten'])})
+					.attr('stroke', 'none')
+					.attr('stroke-width', 3)
+					.attr('opacity', 0.7)
+			
+				d3.selectAll("g.xaxis").remove();
 
-					const xScale = d3.scalePoint().domain(authors).range([100, 750])
-					const yScale = d3.scaleLinear().domain([0, 5]).range([250, 50])
-
-					bubbles
-						.data(curBooks)
-						.join('circle')
-						.attr('cx', function(d){return xScale(d['Author'])})
-						.attr('cy', function(d){return yScale(d['Rating'])})
-						.attr('r', 5)
-						.attr('fill', 'none')
-						.attr('stroke', 'grey')
-						.attr('stroke-width', 3);
+				xaxis = svg.append('g')
+					.attr('transform', 'translate(0, 320)')
+					.call(d3.axisBottom(xScale))
+					.attr("class", "xaxis")
+					.selectAll("text")
+					.style("text-anchor", "end")
+					.attr("dx", "-.8em")
+					.attr("dy", ".15em")
+					.attr("transform", "rotate(-65)")
+					.text(function(d){return d.substring(0,13) + '.'});
 			}
 
 			d3.select("#selectButton").on("change", function() {	
@@ -105,6 +141,40 @@ class ViewC {
 		function removeExtras(data){
 			//remove duplicate values for making selector
 			return data.filter((value, index) => data.indexOf(value) === index);
+		}
+
+		function makeBubble(data, auth){
+			let i = 0;
+			let count = 0;
+			const bubble = new Object();
+			bubble.author = auth;
+			bubble.numRatings = 0;
+			bubble.avgRating = 0;
+			while(i < data.length)
+			{
+				if(data[i]['Author'] == auth)
+				{
+					bubble.numRatings += parseFloat(data[i]['No. of People rated']);
+					bubble.avgRating += parseFloat(data[i]['Rating']);
+					count++;
+				}
+				i++;
+			}
+			bubble.avgRating = (bubble.avgRating)/count
+			bubble.booksWritten = count;
+			return bubble;
+		}
+
+		function makeBubbleList(authors, books)
+		{
+			let i = 0;
+			const bubbleList = [];
+			while(i < authors.length)
+			{
+				bubbleList[i] = makeBubble(books, authors[i])
+				i++;
+			}
+			return bubbleList;
 		}
 
     }
